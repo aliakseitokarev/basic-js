5119c89cb348a88bb0401164d1a652c49b9db726
Alex <aliaksei.tokarev@gmail.com>
2021-03-18 01:10:16 +0300

    feat: add hanoi, transform, chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  tempArr: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.tempArr.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.tempArr.push(`( ${value} )~~`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof(position) != 'number' || position < 1 || position > this.tempArr.length || !(position ^ 0)) {
+      this.tempArr = [];
+      throw new Error();
+    };
+    this.tempArr.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.tempArr.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.tempArr.join('');
+    this.tempArr = [];
+    return result.slice(0, result.length - 2);
   }
 };
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  if (!Array.isArray(arr)) throw new Error ();
+  let result;
+  result = arr.filter((elem, index, array) => {
+    if ((elem === '--double-prev' || elem === '--discard-prev') && (array[index - 2] === '--discard-next')) return false;
+    if ((elem === '--double-next' ) && (array[index + 2] === '--discard-prev')) return false;
+    if ((elem === '--discard-next') || (array[index - 1] === '--discard-next')) return false;
+    if ((elem === '--discard-prev') || ((array[index + 1] === '--discard-prev') && (array[index - 1] != '--double-next'))) return false;
+    if ((elem === '--double-prev') && index == 0) return false;
+    if ((elem === '--double-next') && index == array.length - 1) return false;
+    return true;
+  }).map((currentValue, index, array) => {
+    if (currentValue === '--double-next') return array[index + 1];
+    if (currentValue === '--double-prev') return array[index - 1];
+    return currentValue;
+  });
+
+  return result;
 };
