ece4ce5a409a304db778079eccae9bbb8b01976a
Alex <aliaksei.tokarev@gmail.com>
2021-03-29 01:44:17 +0300

    feat: add vigenere-cipher

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  count = 1;
+  
+  calculateDepth(arr) {
+    let myArr = [];
+    let result;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        this.count++;
+        myArr = arr.flat();
+        this.calculateDepth(myArr);
+        break; 
+      } 
+    }
+    result = this.count;
+    // this.count = 1;
+      return result;
   }
-};
+}
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type) {
+    (type === false) ? this.type = 'reverse' : this.type = 'direct';
+  }
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error();
+    let messageUp = message.toUpperCase();
+    let modifiedKey = key.toUpperCase();
+    let result = '';
+    let count = 0;
+    while (messageUp.length > modifiedKey.length) modifiedKey += key.toUpperCase().repeat(1);
+    for (let index = 0; index < messageUp.length; index++) {
+      if (messageUp[index].charCodeAt() > 64 && messageUp[index].charCodeAt() < 91) {
+        result += String.fromCharCode((messageUp[index].charCodeAt() + modifiedKey[count].charCodeAt() - 130) % 26 + 65);
+        count++;
+      } else result += messageUp[index];
+    };
+    return (this.type === 'reverse' ? result.split('').reverse().join('') : result);
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) throw new Error();
+    let messageUp = encryptedMessage.toUpperCase();
+    let modifiedKey = key.toUpperCase();
+    let result = '';
+    let count = 0;
+    while (messageUp.length > modifiedKey.length) modifiedKey += key.toUpperCase().repeat(1);
+    for (let index = 0; index < messageUp.length; index++) {
+      if (messageUp[index].charCodeAt() > 64 && messageUp[index].charCodeAt() < 91) {
+        result += String.fromCharCode((messageUp[index].charCodeAt() - modifiedKey[count].charCodeAt() + 26) % 26 + 65);
+        count++;
+      } else result += messageUp[index];
+    }
+    return (this.type === 'reverse' ? result.split('').reverse().join('') : result);
+    
   }
 }
 
